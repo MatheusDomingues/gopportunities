9ba89b016abae5eeba83fd79084a2d89a5e2f5bc
MatheusDomingues <matheus_luige@hotmail.com>
2023-11-26 03:28:48 -0300

    feat: add swagger configs and create makefile

--- router/routes.go
+++ router/routes.go
@@ -1,15 +1,20 @@
 package router
 
 import (
+	"github.com/MatheusDomingues/gopportunities/docs"
 	"github.com/MatheusDomingues/gopportunities/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// Initilize Handler
 	handler.InitializeHandler()
+	basePath := "/api/v1"
 
-	v1 := router.Group("/api/v1")
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
@@ -17,4 +22,7 @@ func initializeRoutes(router *gin.Engine) {
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
+
+	// Initialize Swagger
+	router.GET("swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
