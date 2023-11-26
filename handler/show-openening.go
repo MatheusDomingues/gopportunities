7390d040c973a0a3f3d14dc3fe79b09f8bae9b7a
MatheusDomingues <matheus_luige@hotmail.com>
2023-11-26 03:10:05 -0300

    feat: create all logics for openings routes

--- handler/show-openening.go
+++ handler/show-openening.go
@@ -3,11 +3,20 @@ package handler
 import (
 	"net/http"
 
+	"github.com/MatheusDomingues/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadGateway, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+	sendSuccess(ctx, "show-opening", opening)
 }
