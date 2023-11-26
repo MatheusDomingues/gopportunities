453ec6145f27b1058e8bd18876e77f2e59524555
MatheusDomingues <matheus_luige@hotmail.com>
2023-11-26 02:44:18 -0300

    feat: add request and response helpers and createOpening logic handler

--- router/routes.go
+++ router/routes.go
@@ -6,16 +6,15 @@ import (
 )
 
 func initializeRoutes(router *gin.Engine) {
+	// Initilize Handler
+	handler.InitializeHandler()
+
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
-
 		v1.POST("/opening", handler.CreateOpeningHandler)
-
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
-
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
-
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
 }
