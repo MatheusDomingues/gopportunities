8e4c9527020b404da2faea3ea69f98d8b34002ec
MatheusDomingues <matheus_luige@hotmail.com>
2023-11-26 01:21:13 -0300

    feat: add opening schema and logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/MatheusDomingues/gopportunities/router"
+import (
+	"github.com/MatheusDomingues/gopportunities/config"
+	"github.com/MatheusDomingues/gopportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+	// Initialize Config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
